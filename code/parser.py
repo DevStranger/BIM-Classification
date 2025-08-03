82a7821fff5c8a1ea0aa3fd9e06d2a9dfe54eace
DevStranger <143540628+DevStranger@users.noreply.github.com>
2025-08-03 05:57:26 +0200

    Update parser.py

--- code/parser.py
+++ code/parser.py
@@ -3,8 +3,8 @@ import csv
 import os
 
 # Ścieżki do plików
-input_path = "../data/bsdd_cleared.json"                 # plik JSON
-output_path = "../data/tsdae_dataset/file02.csv"         # plik wynikowy CSV
+input_path = "../data/nazwa_pliku.json"                 # plik JSON
+output_path = "../data/nazwa_pliku.csv"                 # plik wynikowy CSV
 
 # Wczytaj dane wejściowe
 with open(input_path, "r", encoding="utf-8") as f:
@@ -50,5 +50,6 @@ with open(output_path, "w", encoding="utf-8", newline="") as csvfile:
     writer.writerows(parsed_rows)
 
 print(f"Zapisano {len(parsed_rows)} komponentów do pliku: {output_path}")
+
 
 
